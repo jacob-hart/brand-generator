0cf7e1422d7b38e0250b8a89bf4bfc06a39bd60b
Jacob <jacobhart1227@gmail.com>
2021-10-09 15:05:28 -0600

    Dynamically update brand name on button press

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function onClick(e) {
   e.preventDefault();
 
-  let value = document.getElementById("weatherInput").value;
-  if (value === "")
-    return;
+  let firstSelector = document.getElementById("firstSelector");
+  let firstSelectorText = firstSelector.options[firstSelector.selectedIndex].text;
+  let secondSelector = document.getElementById("secondSelector");
+  let secondSelectorText = secondSelector.options[secondSelector.selectedIndex].text;
 
-  let url = "https://random-word-form.herokuapp.com/random/" + value
-  fetch(url)
+  let firstUrl = "https://random-word-form.herokuapp.com/random/" + textToQuery(firstSelectorText)
+  fetch(firstUrl)
     .then(function(response) {
       if (response.status != 200) {
         return {
@@ -15,8 +16,37 @@ function onClick(e) {
       }
       return response.json();
     }).then(function(json) {
-      console.log(json)
+      document.getElementById("first").textContent = json[0];
+    });
+
+  let secondUrl = "https://random-word-form.herokuapp.com/random/" + textToQuery(secondSelectorText)
+  fetch(secondUrl)
+    .then(function(response) {
+      if (response.status != 200) {
+        return {
+          text: "Error calling the Numbers API service: " + response.statusText
+        }
+      }
+      return response.json();
+    }).then(function(json) {
+      document.getElementById("second").textContent = json[0];
     });
 }
 
-document.getElementById("weatherSubmit").addEventListener("click", onClick);
+function textToQuery(text) {
+  switch (text)
+  {
+    case "Noun":
+      return "noun";
+    case "Adjective":
+      return "adjective";
+    case "Animal":
+      return "animal";
+    default:
+      throw text + " cannot be converted into a query!"
+  }
+}
+
+
+
+document.getElementById("submit").addEventListener("click", onClick);
